695c865a928cfeaca3baf7b94aa644337e8424d4
Camolesi <Camolesi@CAMOLESI-DELL-5>
2016-09-10 20:44:08 -0300

    atualizar

--- CopiarColarWEB/Default.aspx
+++ CopiarColarWEB/Default.aspx
@@ -19,19 +19,23 @@
     <form id="form1" runat="server">
     <div style="font-size: x-large">
     
-        <span class="auto-style1">Exemplo Copiar Colar<br />
+        <span class="auto-style1"><b>Exemplo Copiar Colar<br />
         <br />
-        Copiar </span>
+        Copiar </b> </span>
         <asp:TextBox ID="TextBox1" runat="server" CssClass="auto-style2"></asp:TextBox>
+        <b>
         <br class="auto-style1" />
-        <span class="auto-style1">Colar </span>
+        </b>
+        <span class="auto-style2">Colar </span>
         <asp:TextBox ID="TextBox2" runat="server" CssClass="auto-style2"></asp:TextBox>
         <span class="auto-style1">
+        <b>
         <br />
         <br />
+        </b>
         </span>
         <asp:Button ID="Button1" runat="server" CssClass="auto-style2" OnClick="Button1_Click" Text="Copiar" />
-        <span class="auto-style1">&nbsp; </span>
+        <span class="auto-style2">&nbsp; </span>
         <asp:Button ID="Button2" runat="server" CssClass="auto-style2" OnClick="Button2_Click" Text="Colar" />
     
     </div>
--- CopiarColarWEB/Default.aspx.cs
+++ CopiarColarWEB/Default.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
-   // private string var1; 
+    //private string var1; 
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -17,13 +17,20 @@ public partial class _Default : System.Web.UI.Page
     protected void Button1_Click(object sender, EventArgs e)
     {
         // var1 = TextBox1.Text;
-        Session["var1"] = TextBox1.Text; 
+        //Session["var1"] = TextBox1.Text; 
+        //Cache["var1"] = TextBox1.Text; 
+        //ViewState["var1"] = TextBox1.Text; 
+        Response.Cookies["var1"].Value = TextBox1.Text;
+        Response.Cookies["var1"].Expires = DateTime.Now.AddDays(30); 
         TextBox1.Text = ""; 
     }
 
     protected void Button2_Click(object sender, EventArgs e)
     {
         //TextBox2.Text = var1; 
-        TextBox2.Text = Session["var1"].ToString(); 
+        //TextBox2.Text = Session["var1"].ToString(); 
+        //TextBox2.Text = Cache["var1"].ToString(); 
+        //TextBox2.Text = ViewState["var1"].ToString(); 
+        TextBox2.Text = Request.Cookies["var1"].Value.ToString(); 
     }
 }
